4df7898fd70d5b4e26e3bd2063bd8ecfef7e38d9
Onur Ozguzel <ozguzel.onur@gmail.com>
2023-09-25 23:03:20 +0200

    Added  first business rule

--- src/RentACar/Application/ApplicationServiceRegistration.cs
+++ src/RentACar/Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Core.Application.Rules;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Application
 {
@@ -7,10 +9,34 @@ namespace Application
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddAutoMapper(typeof(ApplicationServiceRegistration).Assembly);
+            services.AddSubClassesOfType(typeof(ApplicationServiceRegistration).Assembly, typeof(BaseBusinessRules));
             services.AddMediatR(configuration =>
             {
                 configuration.RegisterServicesFromAssemblies(typeof(ApplicationServiceRegistration).Assembly);
             });
+
+            return services;
+        }
+
+        public static IServiceCollection AddSubClassesOfType(
+            this IServiceCollection services,
+            Assembly assembly,
+            Type type,
+            Func<IServiceCollection, Type, IServiceCollection>? addWithLifeCycle = null
+            )
+        {
+            var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
+            foreach (var item in types)
+            {
+                if (addWithLifeCycle == null)
+                {
+                    services.AddScoped(item);
+                }
+                else
+                {
+                    addWithLifeCycle(services, type);
+                }
+            }
 
             return services;
         }
