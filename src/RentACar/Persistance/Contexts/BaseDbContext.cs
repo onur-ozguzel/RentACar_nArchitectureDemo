6128d222f920563584b829c7e98e8106e1d25aa9
Onur Ozguzel <ozguzel.onur@gmail.com>
2024-06-09 16:44:45 +0200

    Upgraded core packages in the main project

--- src/RentACar/Persistance/Contexts/BaseDbContext.cs
+++ src/RentACar/Persistance/Contexts/BaseDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Entities;
+﻿using Core.Security.Entities;
+using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
@@ -13,6 +14,12 @@ namespace Persistance.Contexts
         public DbSet<Fuel> Fuels { get; set; }
         public DbSet<Model> Models { get; set; }
         public DbSet<Transmission> Transmissions { get; set; }
+
+        public DbSet<OperationClaim> OperationClaim { get; set; }
+        public DbSet<OtpAuthenticator> OtpAuthenticators { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
 
         public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
         {
